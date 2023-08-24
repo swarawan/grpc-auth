3074fa2b6bc86fcf3123e1efcb44f0112f08ae40
Rio Swarawan <swarawan.rio@gmail.com>
2023-08-24 15:46:54 +0700

    change method

--- src/main/kotlin/id/swarawan/controller/AuthController.kt
+++ src/main/kotlin/id/swarawan/controller/AuthController.kt
@@ -5,6 +5,7 @@ import id.swarawan.service.AuthService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -15,7 +16,7 @@ class AuthController @Autowired constructor(
 	private val authService: AuthService
 ) {
 
-	@GetMapping(value = ["login"])
+	@PostMapping(value = ["login"])
 	fun login(@RequestBody request: LoginRequest): ResponseEntity<Any> {
 		val auth = authService.login(request)
 		return ResponseEntity.ok(auth)
